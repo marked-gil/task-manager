d1bb88d745f260f73d6cb5af0cdf9785246a5a04
marked-gil <markgil_creates@yahoo.com>
2022-09-17 11:30:06 +0000

    Add comment

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# Since env.py is not pushed to GitHub, this file will not be visible once deployed to Heroku, & will throw an error
+# So, we only need to import env if the OS can find an existing file path for the env.py file itself
 if os.path.exists("env.py"):
     import env   # noqa
-
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
