72999c1724c911ab5704c374ff05c37c59e7490a
marked-gil <markgil_creates@yahoo.com>
2022-09-18 04:56:40 +0000

    Updating categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -17,8 +17,18 @@ def categories():
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
-         category = Category(category_name=request.form.get("category_name"))
-         db.session.add(category)
-         db.session.commit()
-         return redirect(url_for("categories"))
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
