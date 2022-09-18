084c7beb2fd66d8268e493d6724cfb52f013093c
marked-gil <markgil_creates@yahoo.com>
2022-09-18 05:26:05 +0000

    Deleting Categories and using MODALS

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // modal initialization
+    var modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+  });
