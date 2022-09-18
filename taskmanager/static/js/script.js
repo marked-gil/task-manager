9fad8f470bd89e0305c54f55622fa0fcd74a4a66
marked-gil <markgil_creates@yahoo.com>
2022-09-18 07:12:11 +0000

    Adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,12 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
-    var sidenav = document.querySelectorAll('.sidenav');
+    let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
-
-  document.addEventListener('DOMContentLoaded', function() {
     // modal initialization
-    var modal = document.querySelectorAll('.modal');
+    let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
+
+    // Datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
   });
